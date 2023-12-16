ef359c13319a4f4f8b240b91654782dcf3017e0c
Scott Wile <scottpwile@gmail.com>
2023-12-15 21:24:22 -0500

    Day 8

---
+++ 08.r
@@ -0,0 +1,31 @@
+library(stringr)
+
+instructions <- readLines("input_08.txt", n = 1) |>
+    str_split("") |>
+    unlist()
+
+data <- readLines("input_08.txt")[-(1:2)]
+
+map <- data |>
+    str_remove_all("= \\(|,|\\)") |>
+    str_split(" ") |>
+    sapply(\(x) {
+        out <- list(list("L" = x[2], "R" = x[3]))
+        names(out) <- x[1]
+        return(out)
+    })
+
+position <- "AAA"
+step <- 0L
+direction <- 0L
+
+while (position != "ZZZ") {
+    step <- step + 1
+    direction <- direction + 1
+    if (direction > length(instructions)) {
+        direction <- 1
+    }
+    position <- map[[position]][[instructions[direction]]]
+}
+
+step
