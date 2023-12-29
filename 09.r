963a4bb0d2f3ab94db7c395bafdd71032c0011cd
Scott Wile <scottpwile@gmail.com>
2023-12-29 12:02:25 -0500

    Add part 2

--- 09.r
+++ 09.r
@@ -2,7 +2,7 @@ data <- readLines("input_09.txt") |>
     strsplit(" +") |>
     lapply(as.numeric)
 
-get_next_value <- function(data, verbose = FALSE) {
+get_next_value <- function(data, part2 = FALSE) {
     A <- matrix(NA, length(data), length(data), byrow = TRUE)
     A[1, ] <- data
     i <- 1L
@@ -16,9 +16,20 @@ get_next_value <- function(data, verbose = FALSE) {
 
     # Empty rows
     A <- A[rowSums(is.na(A)) != ncol(A), ]
-    if (verbose) print(A)
 
-    return(sum(A[, dim(A)[2]]))
+    if (part2) {
+        x <- diag(A)
+        y <- vector(mode = "numeric", length = length(x))
+        for (i in (length(y) - 1):1) {
+            y[i] <- x[i] - y[i + 1]
+        }
+        return(y[1])
+    } else {
+        return(sum(A[, dim(A)[2]]))
+    }
 }
 
-sum(unlist(lapply(data, get_next_value)))
+c(
+    part1 = sum(unlist(lapply(data, get_next_value))),
+    part2 = sum(unlist(lapply(data, get_next_value, part2 = TRUE)))
+)
